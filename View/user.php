ddf454bd3cd42b515d8363f18230fbe038a90973
HungPhanzzz <hungphan290803@gmail.com>
2023-07-12 16:35:16 +0700

    xong phần login (  khi hồi lỗi xí )

--- View/user.php
+++ View/user.php
@@ -8,6 +8,9 @@ if (isset($_SESSION['username'])) {
 <!DOCTYPE html>
 <html lang="en">
 
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -215,7 +218,7 @@ if (isset($_SESSION['username'])) {
                     <!-- end_container -->
                 </div>
                 <!-- end_pro_head -->
-                <div c ass="product__show container">
+                <div class="product__show container">
                     <div class="row">
                         <?php
                         $conn = get_pdo();
@@ -328,4 +331,4 @@ if (isset($_SESSION['username'])) {
     <!-- end-footer -->
 </body>
 
-</html>
+</html>s
