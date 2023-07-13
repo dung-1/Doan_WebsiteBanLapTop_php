259f31a966f15a6962c74f46fd00f60492f004a0
Dung Paulus CTS <linhtam2001a@gmail.com>
2023-07-13 11:16:11 +0700

    lần này dã xong

--- Admin/view/customer/_index.php
+++ Admin/view/customer/_index.php
@@ -76,4 +76,4 @@
     </div>
     <!-- /.content-wrapper -->
 
-    <?php include_once 'C:/xampp/htdocs/Project-php-mysql/Admin/view/inc/footerAdmin.php' ?>
+    <?php include_once '../view/inc/footerAdmin.php' ?>
