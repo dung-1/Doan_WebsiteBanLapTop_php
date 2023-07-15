ddbcbfbf49a423b912d9cfaf753ffacfb6a2edc3
Dung Paulus CTS <linhtam2001a@gmail.com>
2023-07-16 00:08:34 +0700

    hơi lỗi

--- User/View/xuLyLoginPage.php
+++ User/View/xuLyLoginPage.php
@@ -34,10 +34,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $_SESSION['error'] = 'Mật khẩu không đúng!';
             header('location: loginPage.php');
             exit();
-        }  } else {
-
-            $_SESSION['error'] = 'Tài khoản không tồn tại!';
-            header('location: loginPage.php');
-            exit();
         }
+    } else {
+
+        $_SESSION['error'] = 'Tài khoản không tồn tại!';
+        header('location: loginPage.php');
+        exit();
     }
+}
