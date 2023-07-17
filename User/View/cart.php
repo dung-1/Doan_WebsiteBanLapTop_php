a47c824489808142045337fba9ca8eac4534bd05
HungPhanzzz <hungphan290803@gmail.com>
2023-07-17 17:44:10 +0700

    fix xong validate "thông titn nhận hàng"

--- User/View/cart.php
+++ User/View/cart.php
@@ -113,6 +113,7 @@ if (isset($_POST["update_cart"])) {
     <link rel="stylesheet" href="../../plugins/icons-1.10.5/font/bootstrap-icons.css">
     <link rel="stylesheet" href="css/reponsive.css">
     <link rel="stylesheet" href="css/home.css">
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 </head>
 
 <body>
@@ -120,7 +121,7 @@ if (isset($_POST["update_cart"])) {
 
     <main>
         <div class="container mt-4">
-            <h2><i class="bi bi-basket2 fs-3"></i>Giỏ hàng </h2>
+            <h2><i class="bi bi-basket2 fs-3"></i>Giỏ hàng</h2>
             <hr>
             <?php
             // Hiển thị sản phẩm trong giỏ hàng
@@ -130,7 +131,7 @@ if (isset($_POST["update_cart"])) {
                 $cart_data = json_decode($cookie_data, true);
                 if (!empty($cart_data)) {
             ?>
-                    <form method="post">
+                    <form method="post" id="checkout-form">
                         <table class="table table-bordered cart-table">
                             <thead>
                                 <tr>
@@ -189,7 +190,7 @@ if (isset($_POST["update_cart"])) {
             ?>
 
             <div class="text-center mt-4">
-                <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#emailModal"><i class="bi bi-receipt"></i>Thanh Toán</button>
+                <button class="btn btn-success" id="checkoutButton"><i class="bi bi-receipt"></i>Thanh Toán</button>
             </div>
 
             <!-- Modal -->
@@ -197,30 +198,29 @@ if (isset($_POST["update_cart"])) {
                 <div class="modal-dialog modal-dialog-centered">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="emailModalLabel"> Thông Tin Thanh Toán</h5>
+                            <h5 class="modal-title" id="emailModalLabel">Thông Tin Thanh Toán</h5>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-
                         </div>
                         <div class="modal-body">
                             <div class="mb-3">
-                                <label for="emailInput" class="form-label">Họ và Tên Người Nhận</label>
-                                <input type="text" class="form-control" name="user_name" placeholder="Nhập Tên">
+                                <label for="nameInput" class="form-label">Họ và Tên Người Nhận</label>
+                                <input type="text" class="form-control" id="nameInput" name="user_name" placeholder="Nhập Tên">
                             </div>
                             <div class="mb-3">
                                 <label for="emailInput" class="form-label">Email</label>
-                                <input type="email" class="form-control" id="emailInput" placeholder="Nhập địa chỉ email">
+                                <input type="email" class="form-control" id="emailInput" name="email" placeholder="Nhập địa chỉ email">
                             </div>
                             <div class="mb-3">
-                                <label for="emailInput" class="form-label">Số Điện Thoại </label>
-                                <input type="tel" class="form-control" name="phone" placeholder="Nhập Tên">
+                                <label for="phoneInput" class="form-label">Số Điện Thoại</label>
+                                <input type="tel" class="form-control" id="phoneInput" name="phone" placeholder="Nhập Số Điện Thoại">
                             </div>
                             <div class="mb-3">
-                                <label for="emailInput" class="form-label">Địa chỉ(số nhà/huyện(Phường),tỉnh(Tp))</label>
-                                <input type="text" class="form-control" name="addres" placeholder="Nhập Tên">
+                                <label for="addressInput" class="form-label">Địa chỉ (số nhà/huyện(Phường), tỉnh(Tp))</label>
+                                <input type="text" class="form-control" id="addressInput" name="address" placeholder="Nhập Địa chỉ">
                             </div>
                             <div class="mb-3">
-                                <label for="emailInput" class="form-label">Phương Thức Thanh Toán</label>
-                                <select class="form-select text-capitalize" name="pay" aria-label="Default select example">
+                                <label for="paymentMethodInput" class="form-label">Phương Thức Thanh Toán</label>
+                                <select class="form-select text-capitalize" id="paymentMethodInput" name="payment_method" aria-label="Default select example">
                                     <option selected>Chọn phương thức thanh toán</option>
                                     <option value="1">Chuyển Tiền Qua Momo</option>
                                     <option value="2">Chuyển Tiền Qua Thẻ</option>
@@ -230,138 +230,202 @@ if (isset($_POST["update_cart"])) {
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Đóng</button>
-                            <button type="button" class="btn btn-success buyshop" id="sendInvoiceButton" data-bs-toggle="modal">Thanh Toán</button>
+                            <button type="button" class="btn btn-success" id="sendInvoiceButton">Thanh Toán</button>
                         </div>
                     </div>
                 </div>
             </div>
-
-            
         </div>
     </main>
-    <!-- Trước khi kết thúc thẻ </body> -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
     <script>
-        $(document).ready(function() {
-            // Xử lý sự kiện khi click vào nút "Đồng ý mua"
-            $(".buyshop").click(function() {
-                // Thực hiện gửi Ajax để lưu thông tin đơn hàng
-                $.ajax({
-                    url: "save_invoice.php", // Đường dẫn đến tệp xử lý lưu đơn hàng
-                    type: "POST",
-                    data: {}, // Bạn có thể truyền dữ liệu từ trang này qua Ajax nếu cần thiết
-                    success: function(response) {
-                        // Xử lý phản hồi từ tệp xử lý lưu đơn hàng (nếu cần)
-                        console.log(response);
-                    },
-                    error: function(xhr, status, error) {
-                        // Xử lý lỗi (nếu có)
-                        console.error(error);
+        document.addEventListener('DOMContentLoaded', function() {
+            var itemQuantityInputs = document.querySelectorAll('.item-quantity');
+            itemQuantityInputs.forEach(function(input) {
+                input.addEventListener('blur', function() {
+                    var itemId = input.getAttribute('data-item-id');
+                    var quantity = parseInt(input.value);
+                    if (quantity > 0) {
+                        var itemPrice = parseFloat(input.closest('tr').querySelector('.item-price').innerText.replace(/,/g, ''));
+                        var itemTotalElement = input.closest('tr').querySelector('.item-total');
+                        var itemTotal = quantity * itemPrice;
+                        itemTotalElement.innerText = formatCurrency(itemTotal);
+                        updateCartTotal();
+                    } else {
+                        input.value = 1;
+                        swal("Lỗi", "Số lượng phải lớn hơn 0", "error");
                     }
                 });
             });
+
+            function formatCurrency(amount) {
+                return amount.toLocaleString('en-US', {
+                    minimumFractionDigits: 2,
+                    maximumFractionDigits: 2
+                });
+            }
+
+            var removeItemButtons = document.querySelectorAll('.btn-remove-item');
+            removeItemButtons.forEach(function(button) {
+                button.addEventListener('click', function() {
+                    if (confirm('Bạn có chắc chắn muốn xóa sản phẩm này khỏi giỏ hàng?')) {
+                        var itemId = button.getAttribute('data-item-id');
+                        window.location.href = 'cart.php?id=' + itemId;
+                    }
+                });
+            });
+
+            // Kiểm tra xem thông báo thành công có tồn tại hay không
+            var successMessage = document.getElementById('successMessage');
+            if (successMessage) {
+                // Đặt một độ trễ để loại bỏ thông báo thành công sau 3 giây
+                setTimeout(function() {
+                    successMessage.remove();
+                }, 3000);
+            }
+
+            // cập nhật giỏ hàng
+            function updateCartTotal() {
+                var itemTotals = document.querySelectorAll('.item-total');
+                var total = 0;
+                itemTotals.forEach(function(itemTotal) {
+                    var itemTotalValue = parseFloat(itemTotal.innerText.replace(/,/g, ''));
+                    total += itemTotalValue;
+                });
+                var cartTotalElement = document.getElementById('cart-total');
+                cartTotalElement.innerText = formatCurrency(total);
+            }
+
+            var cartData = <?php echo isset($cart_data) ? json_encode($cart_data) : '[]'; ?>;
+
+            var checkoutButton = document.getElementById('checkoutButton');
+            checkoutButton.addEventListener('click', function() {
+                // Kiểm tra giỏ hàng có rỗng không
+                if (isEmptyCart()) {
+                    swal("Lỗi", "Giỏ hàng rỗng", "error");
+                    return;
+                }
+
+                // Hiển thị modal
+                var emailModal = new bootstrap.Modal(document.getElementById('emailModal'));
+                emailModal.show();
+            });
+
+
+
+
+
+
+
+
+
+
+
+
+            var cartData = <?php echo isset($cart_data) ? json_encode($cart_data) : '[]'; ?>;
+
+            var sendInvoiceButton = document.getElementById('sendInvoiceButton');
+            sendInvoiceButton.addEventListener('click', function() {
+                var nameInput = document.getElementById('nameInput');
+                var emailInput = document.getElementById('emailInput');
+                var phoneInput = document.getElementById('phoneInput');
+                var addressInput = document.getElementById('addressInput');
+                var paymentMethodInput = document.getElementById('paymentMethodInput');
+
+                var name = nameInput.value.trim();
+                var email = emailInput.value.trim();
+                var phone = phoneInput.value.trim();
+                var address = addressInput.value.trim();
+                var paymentMethod = paymentMethodInput.value.trim();
+                // Kiểm tra giỏ hàng có rỗng không
+                if (isEmptyCart()) {
+                    swal("Lỗi", "Giỏ hàng rỗng", "error");
+                    return;
+                }
+
+                // Kiểm tra thông tin thanh toán
+                if (!validatePaymentInfo(name, email, phone, address, paymentMethod)) {
+                    return;
+                }
+                if (email !== '') {
+                    var data = new FormData();
+                    data.append('mail', email);
+                    data.append('subject', 'Hóa đơn mua hàng');
+                    data.append('cart_data', JSON.stringify(cartData));
+                    var xhr = new XMLHttpRequest();
+                    xhr.open('POST', 'sendmail.php', true);
+                    xhr.onload = function() {
+                        if (xhr.status === 200) {
+                            swal("Thành công", "Hóa đơn đã được gửi thành công!", "success").then(function(){
+                            location.reload(); // Tải lại trang để reset giỏ hàn
+                            });
+                        } else {
+                            swal("Lỗi", "Đã xảy ra lỗi khi gửi hóa đơn", "error");
+                        }
+                    };
+                    xhr.send(data);
+                } else {
+                    swal("Lỗi", "Vui lòng nhập địa chỉ email", "error");
+                }
+            });
+
+            function resetCart() {
+                var xhr = new XMLHttpRequest();
+                xhr.onload = function() {
+                    if (xhr.status === 200) {
+                        var response = xhr.responseText;
+                        if (response === 'success') {
+                            location.reload(); // Tải lại trang để reset giỏ hàng
+                        }
+                    }
+                };
+                xhr.send();
+            }
+
+
+
+
+
+
+
+
+
+
+
+            // Kiểm tra giỏ hàng có sản phẩm hay không
+            function isEmptyCart() {
+                var cartData = <?php echo isset($cart_data) ? json_encode($cart_data) : '[]'; ?>;
+                return cartData.length === 0;
+            }
+
+            // Validate thông tin thanh toán
+            function validatePaymentInfo(name, email, phone, address, paymentMethod) {
+                if (name === '') {
+                    swal("Lỗi", "Vui lòng nhập Họ và Tên Người Nhận", "error");
+                    return false;
+                }
+                if (email === '') {
+                    swal("Lỗi", "Vui lòng nhập Email", "error");
+                    return false;
+                }
+                if (phone === '') {
+                    swal("Lỗi", "Vui lòng nhập Số Điện Thoại", "error");
+                    return false;
+                }
+                if (address === '') {
+                    swal("Lỗi", "Vui lòng nhập Địa chỉ", "error");
+                    return false;
+                }
+                if (paymentMethod === '') {
+                    swal("Lỗi", "Vui lòng chọn Phương Thức Thanh Toán", "error");
+                    return false;
+                }
+                return true;
+            }
         });
     </script>
 
     <?php require "footer.php" ?>
 </body>
-
-<script>
-    document.addEventListener('DOMContentLoaded', function() {
-        var itemQuantityInputs = document.querySelectorAll('.item-quantity');
-        itemQuantityInputs.forEach(function(input) {
-            input.addEventListener('blur', function() {
-                var itemId = input.getAttribute('data-item-id');
-                var quantity = parseInt(input.value);
-                if (quantity > 0) {
-                    var itemPrice = parseFloat(input.closest('tr').querySelector('.item-price').innerText.replace(/,/g, ''));
-                    var itemTotalElement = input.closest('tr').querySelector('.item-total');
-                    var itemTotal = quantity * itemPrice;
-                    itemTotalElement.innerText = formatCurrency(itemTotal);
-                    updateCartTotal();
-                } else {
-                    input.value = 1;
-                    alert('Số lượng phải lớn hơn 0');
-                }
-            });
-        });
-
-        function formatCurrency(amount) {
-            return amount.toLocaleString('en-US', {
-                minimumFractionDigits: 2,
-                maximumFractionDigits: 2
-            });
-        }
-        var removeItemButtons = document.querySelectorAll('.btn-remove-item');
-        removeItemButtons.forEach(function(button) {
-            button.addEventListener('click', function() {
-                if (confirm('Bạn có chắc chắn muốn xóa sản phẩm này khỏi giỏ hàng?')) {
-                    var itemId = button.getAttribute('data-item-id');
-                    window.location.href = 'cart.php?id=' + itemId;
-                }
-            });
-        });
-
-        // Kiểm tra xem thông báo thành công có tồn tại hay không
-        var successMessage = document.getElementById('successMessage');
-        if (successMessage) {
-            // Đặt một độ trễ để loại bỏ thông báo thành công sau 3 giây
-            setTimeout(function() {
-                successMessage.remove();
-            }, 3000);
-        }
-        // cập nhật giỏ hàng
-        function updateCartTotal() {
-            var itemTotals = document.querySelectorAll('.item-total');
-            var total = 0;
-            itemTotals.forEach(function(itemTotal) {
-                var itemTotalValue = parseFloat(itemTotal.innerText.replace(/,/g, ''));
-                total += itemTotalValue;
-            });
-            var cartTotalElement = document.getElementById('cart-total');
-            cartTotalElement.innerText = formatCurrency(total);
-        }
-
-        var cartData = <?php echo isset($cart_data) ? json_encode($cart_data) : '[]'; ?>;
-
-        var sendInvoiceButton = document.getElementById('sendInvoiceButton');
-        sendInvoiceButton.addEventListener('click', function() {
-            var emailInput = document.getElementById('emailInput');
-            var email = emailInput.value.trim();
-            if (email !== '') {
-                var data = new FormData();
-                data.append('mail', email);
-                data.append('subject', 'Hóa đơn mua hàng');
-                data.append('cart_data', JSON.stringify(cartData));
-                var xhr = new XMLHttpRequest();
-                xhr.open('POST', 'sendmail.php', true);
-                xhr.onload = function() {
-                    if (xhr.status === 200) {
-                        alert('Hóa đơn đã được gửi thành công');
-                        location.reload(); // Tải lại trang để reset giỏ hàng
-                    } else {
-                        alert('Đã xảy ra lỗi khi gửi hóa đơn');
-                    }
-                };
-                xhr.send(data);
-            } else {
-                alert('Vui lòng nhập địa chỉ email');
-            }
-        });
-
-        function resetCart() {
-            var xhr = new XMLHttpRequest();
-            xhr.onload = function() {
-                if (xhr.status === 200) {
-                    var response = xhr.responseText;
-                    if (response === 'success') {
-                        location.reload(); // Tải lại trang để reset giỏ hàng
-                    }
-                }
-            };
-            xhr.send();
-        }
-    });
-</script>
 
 </html>
